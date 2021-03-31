7941b2e88c8afb3a6622245ce7c7dbec9ec96881
vkponomarev <vkponomarev@gmail.com>
2021-03-31 14:20:10 +0500

    Добавление счетчика

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -154,12 +154,19 @@ AppAsset::register($this);
 
 </footer>
 
+
+
+
 <?php if (Yii::$app->language == 'ru'): ?>
     <script src="https://yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
     <script src="https://yastatic.net/share2/share.js" async="async"></script>
 <?php else: ?>
     <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5dbbf2586b540d45"></script>
 <?php endif; ?>
+
+
+
+
 <script type="text/javascript">
     $('.lightzoom').lightzoom({
         speed: 200,
@@ -169,6 +176,8 @@ AppAsset::register($this);
         isEscClosing: true
     });</script>
 <?php $this->endBody() ?>
+
+<?= $this->render('/partials/counters/_counters.min.php') ?>
 
 </body>
 </html>
--- frontend/views/partials/counters/_counters.php
+++ frontend/views/partials/counters/_counters.php
@@ -1,26 +1,12 @@
 <?php
-
-
-?><!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-    ym(64423438, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true,
-        webvisor:true
-    });
+?>
+<!--LiveInternet counter-->
+<script>
+    new Image().src = "https://counter.yadro.ru/hit?r" +
+        escape(document.referrer) + ((typeof (screen) == "undefined") ? "" :
+            ";s" + screen.width + "*" + screen.height + "*" + (screen.colorDepth ?
+            screen.colorDepth : screen.pixelDepth)) + ";u" + escape(document.URL) +
+        ";h" + escape(document.title.substring(0, 150)) +
+        ";" + Math.random();
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/64423438" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
-
-
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-167846974-1"></script><script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'UA-167846974-1');
-</script>
+<!--/LiveInternet-->
