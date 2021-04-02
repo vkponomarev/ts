c3def7c5713bb42b51af54a3e440777450943ce1
vkponomarev <vkponomarev@gmail.com>
2021-04-02 09:31:26 +0500

    Чистим роуты

--- common/components/gii/giiPDF/GiiPDFGeneratePDFCalendarYearly.php
+++ common/components/gii/giiPDF/GiiPDFGeneratePDFCalendarYearly.php
@@ -46,7 +46,7 @@ class GiiPDFGeneratePDFCalendarYearly
         ];
 
         $count = 0;
-        foreach (range(2019, 2022) as $eachYear) {
+        foreach (range(2010, 2018) as $eachYear) {
 
             foreach ($languagesData as $language) {
                 //$count++;
